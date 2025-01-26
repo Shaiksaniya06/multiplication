9e6c148be4cdff8c7592232e791659253dc76b86
Shaiksaniya06 <sksaniya2006@gmail.com>
2025-01-26 12:31:08 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,11 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        int num1 = 6;
+        int num2 = 6;
+        int result = num1 * num2;
+        System.out.println("the result of multiplication is :" + result);
+    }
+}
+
+    
+
